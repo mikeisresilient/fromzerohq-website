8740fa423715b95b4ce832053ee6e577136332b8
Michael Uchechukwu Ege <michaelege022@gmail.com>
2026-07-30 22:39:08 +0100

    latest

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -74,7 +74,7 @@ function Hero() {
       <Container className="relative z-10 grid items-center gap-20 lg:grid-cols-2">
         {/* LEFT */}
 
-        <motion.div initial="hidden" animate="visible" variants={fadeUp}>
+        <motion.div initial="hidden" animate="visible" variants={fadeUp} className="-mt-8 lg:mt-0">
           <motion.div
             custom={0}
             variants={fadeUp}
